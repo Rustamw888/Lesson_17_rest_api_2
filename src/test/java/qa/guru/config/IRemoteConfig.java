1e1b9fbbaf9a396771268e8b7188508ea64f452c
qaMilord <rustamw888@gmail.com>
2022-06-18 12:38:34 +0300

    homework part 2

--- src/test/java/qa/guru/config/IRemoteConfig.java
+++ src/test/java/qa/guru/config/IRemoteConfig.java
@@ -5,7 +5,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:app/cred.properties"
+        "classpath:app/remote.properties"
 })
 public interface IRemoteConfig extends Config {
 
@@ -13,4 +13,12 @@ public interface IRemoteConfig extends Config {
     String apiUrl();
     String userLogin();
     String userPassword();
+    @DefaultValue("chrome")
+    String browser();
+    @DefaultValue("101.0")
+    String browserVersion();
+    @DefaultValue("1920x1080")
+    String browserSize();
+    String remoteDriverUrl();
+    String videoStorage();
 }
