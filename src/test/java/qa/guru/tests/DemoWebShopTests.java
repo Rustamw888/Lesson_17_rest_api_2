a085f8729f43d46c55535118cd0b0784f6cb85a2
qaMilord <rustamw888@gmail.com>
2022-06-18 05:08:42 +0300

    homework part 2

--- src/test/java/qa/guru/tests/DemoWebShopTests.java
+++ src/test/java/qa/guru/tests/DemoWebShopTests.java
@@ -2,40 +2,36 @@ package qa.guru.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
-import org.junit.jupiter.api.Disabled;
 import org.openqa.selenium.Cookie;
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import qa.guru.config.Remote;
-import qa.guru.helpers.CustomApiListener;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.closeWebDriver;
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.HTML;
 import static qa.guru.helpers.CustomApiListener.withCustomTemplates;
 
 public class DemoWebShopTests extends TestBase {
 
     static String login,
             password,
-            authCookieName = "NOPCOMMERCE.AUTH";
+            authCookieName = "NOPCOMMERCE.AUTH",
+            itemAdd;
 
     @BeforeAll
     static void configureBaseUrl() {
         RestAssured.baseURI = Remote.config.apiUrl();
         Configuration.baseUrl = Remote.config.webUrl();
-
         login = Remote.config.userLogin();
         password = Remote.config.userPassword();
-
     }
 
     @Test
@@ -71,127 +67,49 @@ public class DemoWebShopTests extends TestBase {
                 $(".account").shouldHave(text(login)));
     }
 
+    @Test
+    @Tag("demowebshop")
+    @DisplayName("Shopping card is not empty")
+    void itemAddedToCartTest() {
+        step("Get cookie by api and set it to browser", () -> {
+            String authCookieValue = given()
+                    .filter(withCustomTemplates())
+                    .contentType("application/x-www-form-urlencoded")
+                    .formParam("Email", login)
+                    .formParam("Password", password)
+                    .log().all()
+                    .when()
+                    .post("/login")
+                    .then()
+                    .log().all()
+                    .statusCode(302)
+                    .extract().cookie(authCookieName);
 
+            step("Item added to cart",()->{
+                itemAdd = String.valueOf(given()
+                        .filter(withCustomTemplates())
+                        .cookie(authCookieName, authCookieValue)
+                        .log().all()
+                        .when()
+                        .get("/cart")
+                        .then().log().all()
+                        .statusCode(200)
+                        .contentType(HTML)
+                        .extract().toString().contains("io.restassured.internal.RestAssuredResponseImpl@"));
+            });
 
-//
-//    @Test
-//    @Tag("demowebshop")
-//    @DisplayName("Shopping card is not empty")
-//    void itemAddedToCartTest() {
-//        step("Get cookie by api and set it to browser", () -> {
-//            String authCookieValue = given()
-//                    .filter(withCustomTemplates())
-//                    .contentType("application/x-www-form-urlencoded")
-//                    .formParam("Email", login)
-//                    .formParam("Password", password)
-//                    .log().all()
-//                    .when()
-//                    .post("/login")
-//                    .then()
-//                    .log().all()
-//                    .statusCode(302)
-//                    .extract().cookie(authCookieName);
-//            step("Open minimal content, because cookie can be set when site is opened", () -> {
-//                open("/Themes/DefaultClean/Content/images/logo.png");
-//            });
-//            step("Set cookie to browser", () -> {
-//                Cookie authCookie = new Cookie(authCookieName, authCookieValue);
-//                WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
-//            });
-//            step("Item added to cart", () -> {
-//                open("/cart");
-//            });
-////            String cardCookieValue = given()
-//            Map<String, String> cookies = given()
-//                    .filter(withCustomTemplates())
-////                    .contentType("application/x-www-form-urlencoded")
-////                    .formParam("Email", login)
-////                    .formParam("Password", password)
-//                    .log().all()
-//                    .when()
-//                    .get("/cart")
-//                    .then()
-//                    .log().all()
-//                    .statusCode(200)
-//                    .extract().cookies();
-////                    .extract().cookie();
-////            String cardCookieValue1 = given()
-////                    .post("/card")
-////                    .then()
-////                    .log().all()
-////                    .statusCode(200)
-////                    .extract().cookie(cardCookieName);;
-//
-//            step("Open minimal content, because cookie can be set when site is opened", () -> {
-//                open("/Themes/DefaultClean/Content/images/logo.png");
-//            });
-//            step("Set cookie to browser", () -> {
-////                Cookie cardCookie = new Cookie(cardCookieName, cardCookieValue);
-////                WebDriverRunner.getWebDriver().manage().addCookie(cardCookie);
-//            });
-//        });
-//        step("Item added to cart", () -> {
-//            open("/cart");
-//        });
-//    }
-//
-//    @Test
-//    @Tag("demowebshop")
-//    @DisplayName("Shopping card is not empty")
-//    void itemAddedToCartTest2() {
-//        step("Get cookie by api and set it to browser", () -> {
-////            String authCookieValue = given()
-////                    .filter(withCustomTemplates())
-////                    .contentType("application/x-www-form-urlencoded")
-////                    .formParam("Email", login)
-////                    .formParam("Password", password)
-////                    .log().all()
-////                    .when()
-////                    .post("/login")
-////                    .then()
-////                    .log().all()
-////                    .statusCode(302)
-////                    .extract().cookie(authCookieName);
-////            step("Open minimal content, because cookie can be set when site is opened", () -> {
-////                open("/Themes/DefaultClean/Content/images/logo.png");
-////            });
-////            step("Set cookie to browser", () -> {
-////                Cookie authCookie = new Cookie(authCookieName, authCookieValue);
-////                WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
-////            });
-////            step("Item added to cart", () -> {
-////                open("/cart");
-////            });
-//            String cardCookieValue = given()
-//                    .filter(withCustomTemplates())
-////                    .contentType("application/x-www-form-urlencoded")
-////                    .formParam("Email", login)
-////                    .formParam("Password", password)
-//                    .log().all()
-//                    .when()
-//                    .get("/cart")
-//                    .then()
-//                    .log().all()
-//                    .statusCode(200)
-////                    .extract().cookies();
-//                    .extract().cookie(cardCookieName);
-////            String cardCookieValue1 = given()
-////                    .post("/card")
-////                    .then()
-////                    .log().all()
-////                    .statusCode(200)
-////                    .extract().cookie(cardCookieName);;
-//
-//            step("Open minimal content, because cookie can be set when site is opened", () -> {
-//                open("/Themes/DefaultClean/Content/images/logo.png");
-//            });
-//            step("Set cookie to browser", () -> {
-//                Cookie cardCookie = new Cookie(cardCookieName, cardCookieValue);
-//                WebDriverRunner.getWebDriver().manage().addCookie(cardCookie);
-//            });
-//        });
-//        step("Item added to cart", () -> {
-//            open("/cart");
-//        });
-//    }
+            step("Open minimal content, because cookie can be set when site is opened", () -> {
+                open("/Themes/DefaultClean/Content/images/logo.png");
+            });
+            step("Set cookie to browser", () -> {
+                Cookie authCookie = new Cookie(authCookieName, authCookieValue);
+                WebDriverRunner.getWebDriver().manage().addCookie(authCookie);
+            });
+        });
+        step("Open cart page", () -> {
+            open("/cart");
+        });
+        step("Item successful added to cart", () ->
+                $(".tbody .product-name").find(byText("$100 Physical Gift Card")));
+    }
 }
